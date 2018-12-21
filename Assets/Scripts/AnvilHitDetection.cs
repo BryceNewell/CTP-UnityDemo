5d17b46cdb3b8c1e4b9bb0e8abcccca9b8ecc5a9
Bryce <brycenewell1998@gmail.com>
2018-12-21 16:55:45 +0000

    Started implementation. Grabbing and basic speed detection working

---
+++ Assets/Scripts/AnvilHitDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnvilHitDetection : MonoBehaviour
+{
+    public float measuredSpeed;
+    public float measuredForce;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        measuredSpeed = other.GetComponent<ReadoutPhysics>().currentSpeed;
+        measuredForce = other.GetComponent<ReadoutPhysics>().currentForce;
+    }
+}
---
+++ Assets/Scripts/ReadoutPhysics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadoutPhysics : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float mass;
+
+    public float currentSpeed;
+    public float currentAcceleration;
+    public float currentForce;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = this.gameObject.GetComponentInChildren<Rigidbody>();
+        mass = rb.mass;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentSpeed = rb.velocity.magnitude;
+        currentForce = mass * currentSpeed * 100;
+	}
+}
