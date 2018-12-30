47e9f995d4abc434219f6feb2bced70be3d38337
Bryce <brycenewell1998@gmail.com>
2018-12-30 11:09:00 +0000

    edited some values

---
+++ Assets/Scripts/UINumbers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UINumbers : MonoBehaviour
+{
+    public Text currentHammerSpeed;
+    public Text lastHitSpeed;
+    public Text lastHitForce;
+    public GameObject hammer;
+    public GameObject anvil;
+
+	// Use this for initialization
+	void Start ()
+    {
+        hammer = GameObject.FindGameObjectWithTag("Hammer");
+        anvil = GameObject.FindGameObjectWithTag("Anvil");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentHammerSpeed.text = hammer.GetComponent<ReadoutPhysics>().currentSpeed.ToString();
+        lastHitSpeed.text = anvil.GetComponent<AnvilHitDetection>().measuredSpeed.ToString();
+        lastHitForce.text = anvil.GetComponent<AnvilHitDetection>().measuredForce.ToString();
+	}
+}
