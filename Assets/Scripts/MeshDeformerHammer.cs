762fb2d06abbde3d47a63a3e282a0bdb471a507c
Bryce <brycenewell1998@gmail.com>
2019-02-04 18:36:33 +0000

    Basic interaction between hammer and mesh

---
+++ Assets/Scripts/MeshDeformerHammer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshDeformerHammer : MonoBehaviour
+{
+    public float force ;
+    private GameObject hitObject;
+
+    private void Update()
+    {
+        force = this.GetComponentInParent<ReadoutPhysics>().currentForce;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hitObject = other.gameObject;
+
+        if(hitObject.tag == "Ingot")
+        {
+            HandleInput(hitObject);
+        }
+    }
+
+    void HandleInput(GameObject hitObject)
+    {
+        //Ray inputRay = camera.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hit;
+
+        //if (Physics.Raycast(inputRay, out hit))
+        //{
+        MeshDeformer deformer = hitObject.GetComponent<MeshDeformer>();
+
+        //    if (deformer)
+        //    {
+
+        Vector3 point = transform.position;
+        //point += hit.normal * forceOffset;
+        deformer.AddDeformingForce(point, force);
+
+        //    }
+        //}
+    }
+}
