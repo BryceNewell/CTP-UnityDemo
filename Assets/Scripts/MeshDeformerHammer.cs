c46c97dc42580057c5ce3ab3529babf752c64147
Bryce <brycenewell1998@gmail.com>
2019-02-14 12:34:02 +0000

    added raycast to the hit detection, not working

--- Assets/Scripts/MeshDeformerHammer.cs
+++ Assets/Scripts/MeshDeformerHammer.cs
@@ -6,39 +6,47 @@ public class MeshDeformerHammer : MonoBehaviour
 {
     public float force ;
     private GameObject hitObject;
+    public float forceOffset = 0.1f;
 
     private void Update()
     {
         force = this.GetComponentInParent<ReadoutPhysics>().currentForce * 0.01f;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        hitObject = other.gameObject;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    hitObject = other.gameObject;
 
-        if(hitObject.tag == "Ingot")
+    //    if(hitObject.tag == "Ingot")
+    //    {
+    //        HandleInput(hitObject);
+    //    }
+    //}
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit))
         {
-            HandleInput(hitObject);
+            if (Vector3.Distance(hit.transform.position, transform.position) < 0.5f && hit.transform.gameObject.tag == "Ingot")
+            {
+                HandleInput(hit.transform.gameObject, hit);
+                Debug.Log("Should Deform");
+
+            }
+            Debug.Log("There is something in front of the object!");
+
         }
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.yellow);
     }
 
-    void HandleInput(GameObject hitObject)
+    void HandleInput(GameObject hitObject, RaycastHit hit)
     {
-        //Ray inputRay = camera.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit hit;
-
-        //if (Physics.Raycast(inputRay, out hit))
-        //{
         MeshDeformer deformer = hitObject.GetComponent<MeshDeformer>();
-
-        //    if (deformer)
-        //    {
-
-        Vector3 point = transform.position;
-        //point += hit.normal * forceOffset;
+        Vector3 point = hit.point;
+        point += hit.normal * forceOffset;
         deformer.AddDeformingForce(point, force);
-
-        //    }
-        //}
     }
 }
