c07c43a939a4e51861ad98a38a2cf781058b457b
Bryce <brycenewell1998@gmail.com>
2019-06-05 09:35:35 +0100

    added reset button and made ingot glow

--- Assets/Scripts/ResetIngot.cs
+++ Assets/Scripts/ResetIngot.cs
@@ -6,7 +6,19 @@ public class ResetIngot : MonoBehaviour
 {
     public GameObject ingot;
     public GameObject ingotToSpawn;
+    public GameObject hammerToSpawn;
+    public GameObject wallHammer1;
+    public GameObject wallHammer2;
     public Vector3 spawnLocation;
+    public Vector3 hammerPos1;
+    public Vector3 hammerPos2;
+
+
+    private void Start()
+    {
+        hammerPos1 = wallHammer1.transform.position;
+        hammerPos2 = wallHammer2.transform.position;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +27,14 @@ public class ResetIngot : MonoBehaviour
             Destroy(ingot);
             ingot = Instantiate(ingotToSpawn);
             ingot.transform.position = spawnLocation;
+
+            ingot = Instantiate(ingotToSpawn);
+
+            GameObject hammer1 = Instantiate(hammerToSpawn);
+            hammer1.transform.position = hammerPos1;
+            GameObject hammer2 = Instantiate(hammerToSpawn);
+            hammer2.transform.position = hammerPos2;
+
         }
     }
 }
